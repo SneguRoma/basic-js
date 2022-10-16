5f98fb6f5048dbaf2253ccbc8c620be362cbc68a
sneguroma <sneguroma@gmail.com>
2022-10-16 17:41:32 +0200

    feat: 7 tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  reverse = true;
+  constructor(reverse) {
+    this.reverse = reverse;
+  }
+  
+  
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');  
+  let messCode = '';
+  let keyCode = []; 
+  let message = arguments[0];
+  let key = arguments[1];
+  if (message == undefined || key == undefined) throw new Error('Incorrect arguments!');
+  for (let i = 0;i < key.length; i++){
+    keyCode.push(alphabet.indexOf(key[i].toUpperCase()));    
+  }
+  let keyLength = keyCode.length;
+  let keyCount = 0;
+  
+  for (let i = 0;i < message.length; i++){    
+    let a = alphabet.indexOf(message[i].toUpperCase());
+    let k = (keyCount < keyLength) ? keyCode[keyCount] :  keyCode[keyCount % keyLength] ;
+    let sum = (a + k) < alphabet.length ? a+k :  (a + k) % alphabet.length;    
+    messCode = messCode + ((a === -1 ) ? message[i] : alphabet[sum]);
+    if(a !== -1) keyCount++;
+  }
+  return (!this.reverse) ? messCode : messCode.split("").reverse().join("") ;
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');  
+    let messCode = '';
+    let keyCode = []; 
+    let message = arguments[0];
+    let key = arguments[1];
+    if (message == undefined || key == undefined) throw new Error('Incorrect arguments!');
+     for (let i = 0;i < key.length; i++){
+      keyCode.push(alphabet.indexOf(key[i].toUpperCase()));    
+    }
+    let keyLength = keyCode.length;
+    let keyCount = 0;
+  
+    for (let i = 0;i < message.length; i++){    
+      let a = alphabet.indexOf(message[i].toUpperCase());
+      let k = (keyCount < keyLength) ? keyCode[keyCount] :  keyCode[keyCount % keyLength] ;
+      let sum = (a - k) < 0 ? alphabet.length + (a-k) :  a - k;    
+      messCode = messCode + ((a === -1 ) ? message[i] : alphabet[sum]);
+      if(a !== -1) keyCount++;
+    }
+    return (!this.reverse) ? messCode : messCode.split("").reverse().join("") ;
   }
+  
 }
 
 module.exports = {
