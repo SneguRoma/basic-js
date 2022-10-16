4a760509bb7bb5abfde42a7f69ebf52092f92739
sneguroma <sneguroma@gmail.com>
2022-10-16 12:31:45 +0200

    feat: 5 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain:  [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    
+    this.chain.push('( ' + value + ')');
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(typeof position !== 'number' || position % 1 !==0 || position < 0 || position > this.chain.length) throw new Error ("You can't remove incorrect link!");
+    this.chain.spline(position);
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+    if (this.chain.length === 1) {
+      result = this.chain[0];
+      chain = [];
+      return result;
+    }
+    for (let i = 0; i < this.chain.length -1;i++){
+      result = result + i + '~~';
+    }
+    result = result + this.chain[this.chain.length -1];
+    return result;
   }
 };
 
